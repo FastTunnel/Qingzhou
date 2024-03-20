74774e4e1aa90b6c71e236ab82d958d42ca52022
Gui.H <springhgui@outlook.com>
2024-03-20 21:29:30 +0800

    添加单元测试

---
+++ src/Qz.WebApi/Qz.Application.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Qz.Application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/Qz.WebApi/Qz.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Qz.Domain.Tests
+{
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+
+        }
+
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
