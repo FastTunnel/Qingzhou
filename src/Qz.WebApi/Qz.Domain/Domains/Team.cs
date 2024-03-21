8dca7d3b0863b744690d5e998a7bfbd78ac7b3c2
Gui.H <springhgui@outlook.com>
2024-03-21 18:53:45 +0800

    加入dp思想

--- src/Qz.WebApi/Qz.Domain/Domains/Team.cs
+++ src/Qz.WebApi/Qz.Domain/Domains/Team.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Qz.Domain.DomainPrimitive;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ namespace Qz.Domain.Domains
     {
         public long Id { get; set; }
 
-        public required string Name { get; set; }
+        public required TeamName Name { get; set; }
 
         public required string Description { get; set; }
 
---
+++ src/Qz.WebApi/Qz.Domain/Types/TeamName.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qz.Domain.DomainPrimitive
+{
+    public class TeamName
+    {
+        public TeamName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new ArgumentNullException("name");
+            }
+
+            if (name.Length >= 20)
+            {
+                throw new ArgumentOutOfRangeException(nameof(name), name.Length, "超长");
+            }
+
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+    }
+}
